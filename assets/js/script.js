0dc796e0250a44bbf82b2c9ea961304854a8938a
Stefan R. <ruppe@protonmail.com>
2024-04-19 19:10:32 +0000

    fixed remove last row and styled flex for chart draw buttons. added responsiveness

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,9 +32,9 @@ function titleSwitch() {
   renameBtn.addEventListener("click", function () {
     document.getElementById(
       "title-setter"
-    ).innerHTML = `<label for="title-input">name your chart </label>
+    ).innerHTML = `<label for="title-input">title <i class="fa-solid fa-file-signature"></i></label>
       <input type="text" name="title" id="title-input" class="text-input" />
-      <button id="title-submit-btn" class="btn">submit</button>`;
+      <button id="title-submit-btn" class="btn"><i class="fa-solid fa-check"></i></button>`;
 
     /* setting the EventListener for the "Enter" input repeatedly because it gets lost on DOM change */
     document
@@ -61,37 +61,41 @@ document
 /* Adding and removing rows of the data set */
 
 let plusBtn = document.getElementById("plus-row-btn");
-let dataSetter = document.getElementById('data-setter');
+let dataSetter = document.getElementById("data-setter");
 
-dataSetter.addEventListener('click', function(event){
-  if (event.target.matches('.remove-row-btn')) {
-    removeSpecificRow(event);
+dataSetter.addEventListener("click", function (event) {
+  if (event.target.matches(".remove-row-btn")) {
+    if (document.getElementsByClassName('remove-row-btn').length === 2){
+      removeSpecificRow(event);
+      document.getElementsByClassName('remove-row-btn')[0].remove();
+    } else {
+      removeSpecificRow(event);
+    }
   }
 });
 
 // Removes a specific row from the Data Set by navigation through the
 // DOM via closest <li> item and fetching its index from its parent <ol>
 function removeSpecificRow(event) {
-  let orderedList = document.getElementById('row-list');
+  let orderedList = document.getElementById("row-list");
   let removeBtn = event.target;
-  let removeListItem = removeBtn.closest('li');
+  let removeListItem = removeBtn.closest("li");
   let listItems = Array.from(orderedList.children);
   index = listItems.indexOf(removeListItem);
   dataSet.splice(index, 1);
   removeListItem.remove();
-  
 }
 
 /* row adder */
 plusBtn.addEventListener("click", function () {
   // checking if theres already a button next to first list item
   // then appending it and creating a new row
-  let checkBtn = document.querySelector('.remove-row-btn');
-  if (!(checkBtn)) {
-    let firstRowBtn = document.createElement('button');
-    firstRowBtn.classList="remove-row-btn btn";
-    firstRowBtn.innerText="-row";
-    let firstRow = document.querySelector('ol li');
+  let checkBtn = document.querySelector(".remove-row-btn");
+  if (!checkBtn) {
+    let firstRowBtn = document.createElement("button");
+    firstRowBtn.classList = "remove-row-btn btn";
+    firstRowBtn.innerText = "- row";
+    let firstRow = document.querySelector("ol li");
     if (firstRow) {
       firstRow.appendChild(firstRowBtn);
     }
@@ -102,20 +106,13 @@ plusBtn.addEventListener("click", function () {
   newRow.innerHTML = `
         <input type="text" name="name" class="name text-input" />
         <input type="number" name="value" class="value text-input" />
-        <button class='remove-row-btn btn'>-row</button>
+        <button class='remove-row-btn btn'>- row</button>
     `;
   document.getElementsByTagName("ol")[0].appendChild(newRow);
-
-  
-  
 });
 
-function removeRow() {
-
-}
 
 /* create the data array */
-
 
 let allRows = document.getElementsByClassName("row");
 
@@ -132,37 +129,44 @@ function fetchRow(rowNumber) {
   return [nameData, valueData];
 }
 
-//relic
-let showData = document.getElementById("sd");
-showData.addEventListener("click", function () {
-  dataSet = [];
-  createDataSet(allRows);
-  // giveFeedback();
-  drawChart('pie');
-});
 
-/* choosing the chart type */
-let drawChartBtn = document.getElementsByClassName('draw-chart-btn');
-for ( i = 0; i < drawChartBtn.length; i++) {
-  drawChartBtn[i].addEventListener('click', function(event) {
-    if (event.target.id === 'pie-chart-btn') {
-      dataSet = [];
-      createDataSet(allRows);
-      drawChart('pie');
-    } else if (event.target.id === 'col-chart-btn') {
-      dataSet = [];
-      createDataSet(allRows);
-      drawChart('col');
-    } else if (event.target.id === 'bar-chart-btn') {
-      dataSet = [];
-      createDataSet(allRows);
-      drawChart('bar');
-    }
-  });
-
+// Set DataSet Example
+function setExample() {
+  title = 'Time spend';
+  dataSet = [
+    ["Styling", 3],
+    ["HTML", 1],
+    ["Procrasti-mating", 0.5],
+    ["Research", 3],
+    ["JS", 2],
+  ];
+  return(dataSet);
 }
 
+/* choosing the chart type */
+let drawChartBtn = document.getElementsByClassName("draw-chart-btn");
+for (i = 0; i < drawChartBtn.length; i++) {
+  drawChartBtn[i].addEventListener("click", function (event) {
+    if (event.target.id === "pie-chart-btn") {
+      dataSet = [];
+      createDataSet(allRows);
+      drawChart("pie");
+    } else if (event.target.id === "col-chart-btn") {
+      dataSet = [];
+      createDataSet(allRows);
+      drawChart("col");
+    } else if (event.target.id === "bar-chart-btn") {
+      dataSet = [];
+      createDataSet(allRows);
+      drawChart("bar");
 
+      // Draw Example-Chart
+    } else if (event.target.id === "example-chart-btn") {
+      dataSet = setExample();
+      drawChart("pie");
+    }
+  });
+}
 
 /* dev-div "feedback" for outputting stuff */
 function giveFeedback() {
@@ -178,24 +182,24 @@ function drawChart(chartType) {
   var data = new google.visualization.DataTable();
 
   data.addColumn("string", "Item");
-  data.addColumn("number", "Value");
+  data.addColumn("number", "Amount");
   data.addRows(dataSet);
 
-  var options = { title: title, width: 400, height: 300 };
+  var options = { title: title, width: 800, height: 400 };
 
-  if (chartType === 'pie') {
+  if (chartType === "pie") {
     var chart = new google.visualization.PieChart(
       document.getElementById("chart-div")
     );
-  } else if (chartType === 'col') {
+  } else if (chartType === "col") {
     var chart = new google.visualization.ColumnChart(
       document.getElementById("chart-div")
     );
-  } else if (chartType === 'bar') {
+  } else if (chartType === "bar") {
     var chart = new google.visualization.BarChart(
       document.getElementById("chart-div")
     );
   }
-  
+
   chart.draw(data, options);
 }
