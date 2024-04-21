e00cd9daab8b56a35864e0ab6e0aed2714403068
Stefan R. <ruppe@protonmail.com>
2024-04-21 07:22:24 +0000

    minor styling

---
+++ assets/js/scriptbackup.js
@@ -0,0 +1,242 @@
+/* loading google charts */
+google.charts.load("current", { packages: ["corechart"] });
+// google.charts.setOnLoadCallback(drawChart);
+
+/* initializing the DataSet */
+let dataSet = [];
+/* setting the title of the chart */
+
+/* setting EventListener on button-parent because on button it gets lost on DOM change */
+let titleSetter = document.getElementById("title-setter");
+titleSetter.addEventListener("click", function (event) {
+  if (event.target.matches("#title-submit-btn")) {
+    titleSwitch();
+  }
+});
+
+let title = "generic chart";
+
+function titleSwitch() {
+  title = document.getElementById("title-input").value;
+  document.getElementById("title-setter").innerHTML = `
+                  <span>Chart title:  </span>
+                  <h4 id="set-title">"${title}"</h4>
+                  `;
+  let renameBtn = document.createElement("button");
+  renameBtn.textContent = "rename";
+  renameBtn.classList = "btn";
+  renameBtn.id = "rename-btn";
+  document.getElementById("title-setter").appendChild(renameBtn);
+
+  /* reverse changes if "rename" is clicked */
+  renameBtn.addEventListener("click", function () {
+    document.getElementById(
+      "title-setter"
+    ).innerHTML = `<label for="title-input">title <i class="fa-solid fa-file-signature"></i></label>
+      <input type="text" name="title" id="title-input" class="text-input" />
+      <button id="title-submit-btn" class="btn"><i class="fa-solid fa-check"></i></button>`;
+
+    /* setting the EventListener for the "Enter" input repeatedly because it gets lost on DOM change */
+    document
+      .getElementById("title-input")
+      .addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+          titleSwitch();
+        }
+      });
+  });
+}
+
+/* *click* */
+
+/* *Enter* */
+document
+  .getElementById("title-input")
+  .addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      titleSwitch();
+    }
+  });
+
+/* Adding and removing rows of the data set */
+
+let plusBtn = document.getElementById("plus-row-btn");
+let dataSetter = document.getElementById("data-setter");
+
+dataSetter.addEventListener("click", function (event) {
+  if (event.target.matches(".remove-row-btn")) {
+    if (document.getElementsByClassName('remove-row-btn').length === 2){
+      removeSpecificRow(event);
+      document.getElementsByClassName('remove-row-btn')[0].remove();
+    } else {
+      removeSpecificRow(event);
+    }
+  }
+});
+
+// Removes a specific row from the Data Set by navigation through the
+// DOM via closest <li> item and fetching its index from its parent <ol>
+function removeSpecificRow(event) {
+  let orderedList = document.getElementById("row-list");
+  let removeBtn = event.target;
+  let removeListItem = removeBtn.closest("li");
+  let listItems = Array.from(orderedList.children);
+  index = listItems.indexOf(removeListItem);
+  dataSet.splice(index, 1);
+  removeListItem.remove();
+}
+
+/* row adder */
+plusBtn.addEventListener("click", function () {
+  // checking if theres already a button next to first list item
+  // then appending it and creating a new row
+  let checkBtn = document.querySelector(".remove-row-btn");
+  if (!checkBtn) {
+    let firstRowBtn = document.createElement("button");
+    firstRowBtn.classList = "remove-row-btn btn";
+    firstRowBtn.innerText = "- row";
+    let firstRow = document.querySelector("ol li");
+    if (firstRow) {
+      firstRow.appendChild(firstRowBtn);
+    }
+  }
+
+  let newRow = document.createElement("li");
+  newRow.classList.add("row");
+  newRow.innerHTML = `
+        <input type="text" name="name" class="name text-input" />
+        <input type="number" name="value" class="value text-input" />
+        <button class='remove-row-btn btn'>- row</button>
+    `;
+  document.getElementsByTagName("ol")[0].appendChild(newRow);
+});
+
+
+/* create the data array */
+
+let allRows = document.getElementsByClassName("row");
+
+function createDataSet(rows) {
+  for (let row = 0; row < rows.length; row++) {
+    dataSet.push(fetchRow(row));
+  }
+}
+function fetchRow(rowNumber) {
+  let nameData = document.getElementsByClassName("name")[rowNumber].value;
+  let valueData = parseInt(
+    document.getElementsByClassName("value")[rowNumber].value
+  );
+  return [nameData, valueData];
+}
+
+
+// Set DataSet Example
+function setExample() {
+  title = 'Time spend';
+  dataSet = [
+    ["Styling", 3],
+    ["HTML", 1],
+    ["Procrasti-mating", 0.5],
+    ["Research", 3],
+    ["JS", 2],
+  ];
+  return(dataSet);
+}
+
+/* choosing the chart type */
+let drawChartBtn = document.getElementsByClassName("draw-chart-btn");
+for (i = 0; i < drawChartBtn.length; i++) {
+  drawChartBtn[i].addEventListener("click", function (event) {
+    if (event.target.id === "pie-chart-btn") {
+      dataSet = [];
+      createDataSet(allRows);
+      drawChart("pie");
+    } else if (event.target.id === "col-chart-btn") {
+      dataSet = [];
+      createDataSet(allRows);
+      drawChart("col");
+    } else if (event.target.id === "bar-chart-btn") {
+      dataSet = [];
+      createDataSet(allRows);
+      drawChart("bar");
+
+      // Draw Example-Chart
+    } else if (event.target.id === "example-chart-btn") {
+      dataSet = setExample();
+      drawChart("pie");
+    }
+  });
+}
+
+/* dev-div "feedback" for outputting stuff */
+function giveFeedback() {
+  document.getElementById("feedback").innerText = "Data Set: " + dataSet;
+  console.log(dataSet);
+}
+
+/* ChARTING */
+
+/* setting the chart (customized code snippets from developers.google.com) */
+
+function drawChart(chartType) {
+  var data = new google.visualization.DataTable();
+
+  data.addColumn("string", "Item");
+  data.addColumn("number", "Amount");
+  data.addRows(dataSet);
+
+  var options = { title: title, width: 800, height: 400 };
+
+  if (chartType === "pie") {
+    var chart = new google.visualization.PieChart(
+      document.getElementById("chart-div")
+    );
+  } else if (chartType === "col") {
+    var chart = new google.visualization.ColumnChart(
+      document.getElementById("chart-div")
+    );
+  } else if (chartType === "bar") {
+    var chart = new google.visualization.BarChart(
+      document.getElementById("chart-div")
+    );
+  }
+
+  chart.draw(data, options);
+  document.getElementById('chart-div').scrollIntoView({
+    behavior: 'smooth',
+    block: 'start',
+    inline: 'nearest'
+  });
+}
+
+/* Landscape Orientation for mobile (code mostly from chatGPT) */
+if ('orientation' in screen) {
+  let chartDiv = document.getElementById('chart-div');
+  let observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        screen.orientation.lock('landscape-primary')
+        .then(() => {
+          console.log('Orientation locked to landscape');
+        })
+        .catch((error) => {
+          console.error('Failed to lock orientation: ', error);
+        });
+      } else {
+        screen.orientation.unlock()
+          .then(() => {
+            console.log('Orientation unlocked');
+          })
+          .catch((error) => {
+            console.error('Failed to unlock orientation: ', error);
+          });
+        }
+    })
+  });
+  
+  observer.observe(chartDiv);
+} else {
+    console.error('Screen Orientation API not supported');
+};
+
+
